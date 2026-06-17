76efbb6b29f1e81680ec7a61d42a0d992d85162a
Rohan_Rajak <129177638+Rohan-Rajak07@users.noreply.github.com>
2026-06-17 05:43:27 +0000

    tesing codesandbox

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
-app.listen(3000,()=>{
-    console.log("Server is running on http://localhost:3000");
+app.listen(5000,()=>{
+    console.log("Server is running on http://localhost:5000");
 });
