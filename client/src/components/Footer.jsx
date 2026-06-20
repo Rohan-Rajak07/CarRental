58c4259d9351e86df05e04e4007276222dee8b22
Rohan-Rajak07 <rohanrajak132@gmail.com>
2026-06-20 13:05:59 +0530

    Added Routes

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center bg-dark text-light p-3">
+      <h4>All right reserved &copy; Techinfoyt </h4>
+        <h4>made with From India 💖</h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Header = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo01"
+                        aria-controls="navbarTogglerDemo01"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <Link className="navbar-brand" to="/">
+                            <i className='fa fa-car'></i>Car Rental App
+                        </Link>
+
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/cars">
+                                    All Cars
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/about">
+                                    About
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/contact">
+                                    Contact
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/register">
+                                    Register
+                                </Link>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
